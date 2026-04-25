08ab7d9f2f7ea78b6395d55a2b71f8ab955977af
linshmin <153379365+linshmin@users.noreply.github.com>
2026-04-25 22:53:09 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,40 +1,32 @@
-// 初始化
+function showPage(p) {
+  document.querySelectorAll(".page").forEach(el=>el.classList.add("hidden"));
+  document.getElementById(p).classList.remove("hidden");
+}
+
 if (!localStorage.posts) localStorage.posts = "[]";
 if (!localStorage.works) localStorage.works = "[]";
 
-// 切頁
-function showPage(page) {
-  document.querySelectorAll(".page").forEach(p=>p.classList.add("hidden"));
-
-  if (page=="home") homePage.classList.remove("hidden");
-  if (page=="post") postPage.classList.remove("hidden");
-  if (page=="work") workPage.classList.remove("hidden");
-}
-
-// 新增文章
-function addPost() {
+function addPost(){
   let posts = JSON.parse(localStorage.posts);
 
   posts.push({
-    title: title.value,
-    content: content.value,
-    date: new Date().toLocaleDateString()
+    title:title.value,
+    content:content.value,
+    date:new Date().toLocaleDateString()
   });
 
   localStorage.posts = JSON.stringify(posts);
   loadPosts();
 }
 
-// 載入文章
-function loadPosts() {
+function loadPosts(){
   let posts = JSON.parse(localStorage.posts);
 
-  postList.innerHTML = "";
-  allPosts.innerHTML = "";
+  postList.innerHTML="";
+  allPosts.innerHTML="";
 
   posts.forEach(p=>{
-    let html = `
-    <div class="card">
+    let html = `<div class="card">
       <b>${p.title}</b><br>
       ${p.content}<br>
       <small>${p.date}</small>
@@ -44,31 +36,27 @@ function loadPosts() {
     allPosts.innerHTML += html;
   });
 
-  count1.innerText = posts.length;
+  count.innerText = posts.length;
 }
 
-// 工作紀錄
-function addWork() {
+function addWork(){
   let works = JSON.parse(localStorage.works);
 
-  works.push({
-    content: workInput.value
-  });
+  works.push({content:workInput.value});
 
   localStorage.works = JSON.stringify(works);
   loadWorks();
 }
 
-function loadWorks() {
+function loadWorks(){
   let works = JSON.parse(localStorage.works);
 
-  workList.innerHTML = "";
+  workList.innerHTML="";
 
   works.forEach(w=>{
     workList.innerHTML += `<div class="card">${w.content}</div>`;
   });
 }
 
-// 初始化載入
 loadPosts();
 loadWorks();
